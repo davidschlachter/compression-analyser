8db8aafb615bb623aaa838f689035693628e9ee2
David Schlachter <david@schlachter.ca>
2020-03-21 22:25:09 -0400

    Identify longest linear region

--- analyser.py
+++ analyser.py
@@ -66,7 +66,7 @@ def main(argv):
 	print("Displ.\tForce")
 	firstDerivatives = [0, 0]
 	secondDerivative = 0
-	secondDerivatives = [[], [], [], []] # displacement, second derivative, displacement range, force range
+	secondDerivatives = [[], [], [], []] # displacement, second derivative, average displacement, average force
 	minMaxPoints = [[], []]
 	for i in range(2, len(forceSet)):
 		if forceSet[i] < 0.5: # false positives in unstable region before test begins
@@ -78,8 +78,8 @@ def main(argv):
 		firstDerivatives.append(deriv2)
 		secondDerivatives[0].append(displacementSet[i-1])
 		secondDerivatives[1].append(secondDerivative)
-		secondDerivatives[2].append([ displacementSet[i-2], displacementSet[i] ])
-		secondDerivatives[3].append([ forceSet[i-2], forceSet[i] ])
+		secondDerivatives[2].append( (displacementSet[i-2] + displacementSet[i])/2 )
+		secondDerivatives[3].append( (forceSet[i-2] + forceSet[i])/2 )
 		if (deriv1 * deriv2) < 0:
 			print(displacementSet[i-1], forceSet[i-1])
 			minMaxPoints[0].append(displacementSet[i-1])
@@ -91,25 +91,56 @@ def main(argv):
 	linearPortion = []
 	for i in range(0, len(secondDerivatives[0])):
 		if abs(secondDerivatives[1][i]) < threshold:
-			x1 = secondDerivatives[2][i][0]
-			y1 = secondDerivatives[3][i][0]
-			x2 = secondDerivatives[2][i][1]
-			y2 = secondDerivatives[3][i][1]
-			linearPortion.append([[x1, x2], [y1, y2]])
+			linearPortion.append([ secondDerivatives[2][i], secondDerivatives[3][i] ])
 	
-	# Find the longest linear region before the global maximum
+	# Find the global maximum
 	global_max_y = max(forceSet)
 	global_max_x = displacementSet[forceSet.index(global_max_y)]
 	print("\n# Global max:\n" + str(global_max_x) + "\t" + str(global_max_y))
-	
+
+	# Merge together the linear regions (currently stored as individual points)
+	lossyLinearRegion = []
+	stepSize = 2 * abs(displacementSet[1] - displacementSet[0])
+	print("stepSize: ", stepSize)
+	x1, y1, x2, y2 = linearPortion[0][0], linearPortion[0][1], 0, 0
+	for i in range(1, len(linearPortion)):
+		if abs(linearPortion[i][0] - linearPortion[i-1][0]) > (stepSize + 0.000001): # add small value to overcome float precision problems
+			x2 = linearPortion[i-1][0]
+			y2 = linearPortion[i-1][1]
+			lossyLinearRegion.append([ [x1, x2], [y1, y2] ])
+			x1 = linearPortion[i][0]
+			y1 = linearPortion[i][1]
+
+	# Identify the longest linear region before the global maximum
+	longestRange, longestRangeSet, longestRangeIndex = 0, [], 0
+	for i in range(0, len(lossyLinearRegion)):
+		if lossyLinearRegion[i][1][1] < global_max_y:
+			if (lossyLinearRegion[i][0][1] - lossyLinearRegion[i][0][0]) > longestRange:
+				longestRange = lossyLinearRegion[i][0][1] - lossyLinearRegion[i][0][0]
+				longestRangeSet = lossyLinearRegion[i].copy()
+				longestRangeIndex = i
+	del lossyLinearRegion[longestRangeIndex]
+
+	# Calculate the slope of the longest linear region
+	print("\nMaximum yield stress:")
+	x1, y1, x2, y2 = longestRangeSet[0][0], longestRangeSet[1][0], longestRangeSet[0][1], longestRangeSet[1][1]
+	print(y2)
+	slope = (y2 - y1)/(x2 - x1)
+	print("Modulus:")
+	print(slope)
 
 	# Plot the results
 	plt.plot(displacementSet, forceSet, label="force")
 	plt.plot(displacementSet, firstDerivatives, label="first derivative")
 	plt.plot(secondDerivatives[0], secondDerivatives[1], label="second derivative")
 	plt.plot(minMaxPoints[0], minMaxPoints[1], "o", label="local minima and maxima")
-	for i in linearPortion:
-		plt.plot(i[0], i[1], "o-", color="purple", label="")
+	for i in range(0, len(lossyLinearRegion)):
+		if i == 0:
+			thisLabel = "linear regions"
+		else:
+			thisLabel = ""
+		plt.plot(lossyLinearRegion[i][0], lossyLinearRegion[i][1], "o-", color="purple", label=thisLabel)
+	plt.plot(longestRangeSet[0], longestRangeSet[1], "o-", color="magenta", label="Modulus: "+str(round(slope,2)))
 	plt.legend(loc=0)
 	plt.show()
 
